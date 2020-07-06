1094d4bee194ddad6313ec626595f13f0e93db2e
eduardo martinez <chente31209@hotmail.com>
2020-07-06 14:25:14 -0500

    se añadio el prollecto using testing

---
+++ MyLibraryTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace MyLibraryTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CategoriesCatalogTest()
+        {
+            
+
+        }
+    }
+}
